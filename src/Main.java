2a6de1fdaba0babfd2b71124b6a4740323bd5c18
ilkaypolat1985 <ilkaypolat1985@gmail.com>
2019-12-23 15:46:23 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("******************************");
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i++);
+        }
+
+        System.out.println();
+        System.out.println("******************************");
+        i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i += 2;
+        }
+
+        System.out.println();
+        System.out.println("******************************");
+        i = 2;
+        while (i < 10) {
+            System.out.println(i);
+            i += 2;
+        }
+
+
+    }
+}
